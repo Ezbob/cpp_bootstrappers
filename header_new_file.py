ead838dce0ab67a9f955760f4b3a9c656941cd27
Anders Busch <andersbusch@gmail.com>
2019-11-14 11:37:17 +0100

    added header file generator python implementation

---
+++ header_new_file.py
@@ -0,0 +1,58 @@
+#!env python
+"""
+    Header file generator.
+    Requires python 3
+"""
+import pathlib
+import argparse
+import uuid
+import sys
+from datetime import date
+
+ROOT_DIR = pathlib.Path(__file__).parent
+
+def die(error_msg, error_code=1):
+    print("Error: {}".format(error_msg), file=sys.stderr)
+    exit(error_code)
+
+def parse_cli_args():
+    parser = argparse.ArgumentParser(description="header generator tool for c/c++")
+    parser.add_argument("new_filepath", type=pathlib.Path)
+    return parser.parse_args()
+
+def get_license_lines(replacements, license_filename="mit", delimiter="//"):
+    with (ROOT_DIR / "license_templates" / license_filename).open(mode="r") as license_file:
+        for line in license_file:
+            result_line = line
+            for key, value in replacements.items():
+                if key in line:
+                    result_line = line.replace(key, str(value))
+            if len(result_line.strip()) == 0:
+                yield "{}\n".format(delimiter)
+            else:
+                yield "{} {}".format(delimiter, result_line)
+    yield "\n\n"
+
+def main(argv):
+    if argv.new_filepath.exists(): die("File already exists. Stopping")
+
+    replacement_parameters = {
+        "@@CURRENT_YEAR@@": date.today().year
+    }
+
+    with argv.new_filepath.open(mode="w+") as out_file:
+        out_file.writelines(get_license_lines(replacements=replacement_parameters))
+
+        header_symbol = "_HEADER_GUARD_{}".format(uuid.uuid4().hex)
+
+        out_file.writelines([
+            "#ifndef {}\n".format(header_symbol),
+            "#define {}\n".format(header_symbol),
+            "\n",
+            "// add you code here\n",
+            "\n",
+            "#endif\n"
+        ])
+
+if __name__ == "__main__":
+    main(parse_cli_args())
