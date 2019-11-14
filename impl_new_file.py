9f0ea1683949c3ff6ee57c34191aac4ec0b51a2e
Anders Busch <andersbusch@gmail.com>
2019-11-14 13:02:08 +0100

    implementation file generator for C/C++ in python added

--- impl_new_file.py
+++ impl_new_file.py
@@ -1,11 +1,10 @@
 #!env python
 """
-    Header file generator for C/C++.
+    Implementation file generator for C/C++.
     Requires python 3
 """
 import pathlib
 import argparse
-import uuid
 import sys
 from datetime import date
 
@@ -18,7 +17,9 @@ def die(error_msg, error_code=1):
 def parse_cli_args():
     parser = argparse.ArgumentParser(description="header generator tool for c/c++")
     parser.add_argument("new_filepath", type=pathlib.Path)
-    parser.add_argument("-l", "--license", type=str, default="mit", choices={"mit", "none"})
+    parser.add_argument("-l", "--license", type=str, default="mit", choices={"mit", "none"}, help="Which license to preppend to the new file")
+    parser.add_argument("-n", "--no-include", action="store_true", help="Do not generate a include preprocessor statement")
+    parser.add_argument("-c", "--using-c", action="store_true", help="Header includes will be prefixed with .h instead of .hpp to be compatible with C")
     return parser.parse_args()
 
 def get_license_lines(replacements, license_filename, delimiter="//"):
@@ -45,15 +46,17 @@ def main(argv):
         if argv.license != "none":
             out_file.writelines(get_license_lines(replacements=replacement_parameters, license_filename=argv.license))
 
-        header_symbol = "_HEADER_GUARD_{}".format(uuid.uuid4().hex)
+        if not argv.no_include:
+            filename = argv.new_filepath.stem # this is the filename of the new file without extension
+            if argv.using_c:
+                out_file.write("#include \"{}.h\"\n".format(filename))
+            else:
+                out_file.write("#include \"{}.hpp\"\n".format(filename))
 
         out_file.writelines([
-            "#ifndef {}\n".format(header_symbol),
-            "#define {}\n".format(header_symbol),
             "\n",
             "// add your code here\n",
-            "\n",
-            "#endif\n"
+            "\n"
         ])
 
 if __name__ == "__main__":
