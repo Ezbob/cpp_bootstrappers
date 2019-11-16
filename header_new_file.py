4a1d864f7d286eb27a4e44694784023507893bcd
Anders Busch <andersbusch@gmail.com>
2019-11-16 12:47:33 +0100

    refactor of python files

--- header_new_file.py
+++ header_new_file.py
@@ -11,9 +11,11 @@ from datetime import date
 
 ROOT_DIR = pathlib.Path(__file__).parent
 
+
 def die(error_msg, error_code=1):
     print("Error: {}".format(error_msg), file=sys.stderr)
     exit(error_code)
+
 
 def parse_cli_args():
     parser = argparse.ArgumentParser(description="header generator tool for c/c++")
@@ -21,18 +23,28 @@ def parse_cli_args():
     parser.add_argument("-l", "--license", type=str, default="mit", choices={"mit", "none"})
     return parser.parse_args()
 
+
+def get_replaced_lines(stream, replacements, delimiter="//"):
+    """
+    Go through a file stream, line by line, and fill out the template variables.
+    This is a line generator.
+    """
+    for line in stream:
+        result_line = line
+        for key, value in replacements.items():
+            if key in line:
+                result_line = line.replace(key, str(value))
+        if len(result_line.strip()) == 0:
+            yield "{}\n".format(delimiter)
+        else:
+            yield "{} {}".format(delimiter, result_line)
+
+
 def get_license_lines(replacements, license_filename, delimiter="//"):
     with (ROOT_DIR / "license_templates" / license_filename).open(mode="r") as license_file:
-        for line in license_file:
-            result_line = line
-            for key, value in replacements.items():
-                if key in line:
-                    result_line = line.replace(key, str(value))
-            if len(result_line.strip()) == 0:
-                yield "{}\n".format(delimiter)
-            else:
-                yield "{} {}".format(delimiter, result_line)
+        yield from get_replaced_lines(license_file, replacements, delimiter)
     yield "\n\n"
+
 
 def main(argv):
     if argv.new_filepath.exists(): die("File already exists. Stopping")
--- impl_new_file.py
+++ impl_new_file.py
@@ -10,9 +10,11 @@ from datetime import date
 
 ROOT_DIR = pathlib.Path(__file__).parent
 
+
 def die(error_msg, error_code=1):
     print("Error: {}".format(error_msg), file=sys.stderr)
     exit(error_code)
+
 
 def parse_cli_args():
     parser = argparse.ArgumentParser(description="header generator tool for c/c++")
@@ -22,18 +24,28 @@ def parse_cli_args():
     parser.add_argument("-c", "--using-c", action="store_true", help="Header includes will be prefixed with .h instead of .hpp to be compatible with C")
     return parser.parse_args()
 
+
+def get_replaced_lines(stream, replacements, delimiter="//"):
+    """
+    Go through a file stream, line by line, and fill out the template variables.
+    This is a line generator.
+    """
+    for line in stream:
+        result_line = line
+        for key, value in replacements.items():
+            if key in line:
+                result_line = line.replace(key, str(value))
+        if len(result_line.strip()) == 0:
+            yield "{}\n".format(delimiter)
+        else:
+            yield "{} {}".format(delimiter, result_line)
+
+
 def get_license_lines(replacements, license_filename, delimiter="//"):
     with (ROOT_DIR / "license_templates" / license_filename).open(mode="r") as license_file:
-        for line in license_file:
-            result_line = line
-            for key, value in replacements.items():
-                if key in line:
-                    result_line = line.replace(key, str(value))
-            if len(result_line.strip()) == 0:
-                yield "{}\n".format(delimiter)
-            else:
-                yield "{} {}".format(delimiter, result_line)
+        yield from get_replaced_lines(license_file, replacements, delimiter)
     yield "\n\n"
+
 
 def main(argv):
     if argv.new_filepath.exists(): die("File already exists. Stopping")
